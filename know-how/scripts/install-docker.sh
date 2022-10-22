12fae4bfeb0db0eb4565f854312254d38e4d2f41
mariamonzon <50300669+mariamonzon@users.noreply.github.com>
2022-10-22 20:44:11 +0200

    include docker hello-word to test installation

--- know-how/scripts/install-docker.sh
+++ know-how/scripts/install-docker.sh
@@ -24,3 +24,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
+# Test if the installation works
+docker run hello-world
